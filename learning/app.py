e3bda6aaa49b216e50111df7861d91da6a7142eb
Leah Deshar <leah.desar@gmail.com>
2024-01-11 21:36:18 +0545

    custom tkinter

--- learning/app.py
+++ learning/app.py
@@ -1,18 +1,17 @@
-import tkinter as tk
 
 
-root = tk.Tk()
-root.title("my app")
-label_options = {
-    "text": "Hello, Tkinter!",
-    "font": ("Arial", 16),
-    "fg": "blue",  # Foreground color
-    "bg": "lightgray",  # Background color
-    "width": 20,
-    "height": 3,
-}
-tk.Label(root,**label_options).pack()
-# tk.Label(root,text="Hello World!").pack()
+import tkinter
+import customtkinter as tk
 
+root_tk = tkinter.Tk()  
+root_tk.geometry("400x240")
+root_tk.title("CustomTkinter Test")
 
-root.mainloop()
+def button_function():
+    print("button pressed")
+
+# Use CTkButton instead of tkinter Button
+button = tk.CTkButton(master=root_tk, corner_radius=10, command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+root_tk.mainloop()
